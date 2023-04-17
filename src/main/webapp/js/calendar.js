1ba006695e0c9c396557e34ab4527d6163f9d875
Stephanie Be <besa@usc.edu>
2023-04-16 21:13:00 -0700

    small html/js things
    tiny bug i found in eventDisplay.html
    added some button functionalities (dont work right now)
    added registration js file
    edited registration.html

--- src/main/webapp/js/calendar.js
+++ src/main/webapp/js/calendar.js
@@ -1,11 +1,14 @@
-/**
- * 
+/**function to add new event(move to eventDisplay.html)
+ * function to move to login.html
+ * function to display all events on day that was clicked?
  */
 
  function AddEvent() {
-	 
+	 e.preventDefault();
+	 window.location.href = 'addEvent.html';
  }
  
  function LoginRegister() {
-	 
+	 e.preventDefault();
+	 window.location.href = 'login.html';
  }
--- src/main/webapp/js/eventDisplay.js
+++ src/main/webapp/js/eventDisplay.js
@@ -1,8 +1,9 @@
-/**
- * 
+/**function to move back to the calendar(?) screen
+ * use GET(?)
  */
 
 //Implementation of the back button.
  function OnBack() {
-	 
- }
+	 e.preventDefault();
+	 window.history.back();
+ };
