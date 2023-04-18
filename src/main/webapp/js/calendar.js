2ad388c44ec94b129bcbd5beda7953ca06301a9b
Stephanie Be <besa@usc.edu>
2023-04-17 23:41:36 -0700

    prelim code for calendar and eventDisplay js

--- src/main/webapp/js/calendar.js
+++ src/main/webapp/js/calendar.js
@@ -1,14 +1,44 @@
-/**function to add new event(move to eventDisplay.html)
- * function to move to login.html
- * function to display all events on day that was clicked?
- */
+//including ajax for jQuery
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
 
+//function to go to add event page
  function AddEvent() {
 	 e.preventDefault();
 	 window.location.href = 'addEvent.html';
  }
  
+ //function to go to login register page
  function LoginRegister() {
 	 e.preventDefault();
 	 window.location.href = 'login.html';
  }
+ 
+ //function to display event information on a current day
+ //use ajax to get information from servlet + json strings
+ //use for loop to print out event information in divs
+async function DisplayEventsInformation(){
+	 var baseURL = window.location.origin + "/USCheduler/";
+	 var url = new URL("EventListServlet", baseURL);
+	 fetch(url)
+		 .then(data => {
+			 console.log(data.text())
+			 return data.json();
+		 })
+		 .then(data => {
+			 Object.keys(data).forEach(function(key){
+				 //create a new div and display it!!
+				 var newDiv = document.createElement("div");
+				 //newDiv.textContent = ;
+				 var eventName = document.createElement("p");
+				 eventName.innerText = data['name'];
+				 newDiv.appendChild(eventName);
+				 var eventDetails = document.createElement("p");
+				 eventDetails.innerText = data['organizer'] + data['time'] + '-' + data['time-end'];
+				 newDiv.appendChild(eventDetails);
+			 	 //insert div into html//gotta give the class scroll the id scroll
+			 	 document.getElementById('scroll').appendChild(newDiv);
+			 }); 
+		 }).catch(function(error){
+			 console.log('error in loading event info', error)
+		 });
+}
--- src/main/webapp/js/eventDisplay.js
+++ src/main/webapp/js/eventDisplay.js
@@ -1,9 +1,67 @@
-/**function to move back to the calendar(?) screen
- * use GET(?)
- */
+//untested, rough drafted code
+//including ajax for jQuery
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
 
 //Implementation of the back button.
  function OnBack() {
 	 e.preventDefault();
 	 window.history.back();
- };
+ }
+ 
+ //function to display current event, organizer, date/time, event description, and image
+ //but how to get specific event? from event_id? how to get this info?
+ function loadEventInformation(){
+	 //try with fetch
+	 var baseURL = window.location.origin + "/USCheduler/";
+	 var url = new URL("EventListServlet", baseURL);
+	 
+	 fetch(url)
+		 .then(data => {
+			 console.log(data.text())
+			 return data.json();
+		 })
+		 .then(data => {
+			 document.getElementById('title').innerHTML = data['name'];
+			 document.getElementById('club').innerHTML = data['organizer'];
+			 document.getElementById('time').innerHTML = data['date'] + ' | ' + data['time'] + '-' + data['time-end'];
+			 document.getElementById('description').innerHTML = data['description'];
+			 document.getElementById('imgFrame').innerHTML = data['time']; //change this later
+		 }).catch(function(error){
+			 console.log('error in loading event info', error)
+		 });
+		 
+	 /* failed attempts, will delete late but just want the code on record
+	 //try with ajax
+	 $.ajax({
+		url: "EventListServlet",
+		data: {
+			title: document.getElementById('title'),
+			field: "title", //name
+			club: document.getElementById('club'),
+			field: "club", //organizer
+			time: document.getElementById('time'),
+			field: "time", //date | time - time end
+			decription: document.getElementById('description'),
+			field: "description", //description
+			imgFrame: document.getElementById('imgFrame'),
+			field: "imgFrame" //?
+		},
+		success: function(result){
+			$("#title").html(result);
+			$("#club").html(result);
+			$("#time").html(result);
+			$("#description").html(result);
+			$("#imgFrame").html(result);
+		}
+	 });
+	 
+	 //try with xhttp
+	 let baseURL = window.location.origin;
+	 const xhttp = new XMLHttpRequest();
+	 xhttp.onload = function(){
+		 document.getElementById("").innerHTML = this.responseText;
+	 }
+	 xhttp.open("GET", "url", true);
+	 xhttp.send();
+	 */
+ }
